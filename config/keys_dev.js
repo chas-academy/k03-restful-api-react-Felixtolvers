db476b53e8ba16d047785a99d5757b106efd526b
Felix Tolvers <felix.tolvers@desenio.com>
2019-03-14 15:29:04 +0100

    Connected to databse and added routes for items

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://Felix:felix123@cluster0-mnn4u.mongodb.net/test?retryWrites=true",
+  secretOrKey: "SECRET"
+};
--- server.js
+++ server.js
@@ -2,8 +2,28 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+const items = require("./routes/api/items");
+
 const app = express();
 
 // Bodyparser Middleware
 
 app.use(bodyParser.json());
+
+// DB Config
+
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
+
+// Use Routes
+app.use("/api/items", items);
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
